a0c6a1e0f2ddc593cd1b59287d9079cda4397e10
Lucas Navarro <navarrolucas0009@gmail.com>
2024-10-25 11:04:16 -0300

    branch

---
+++ relacionamentos-sequelize/db/connection.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('sequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+// Só da uma mensagem de SUCCESS ou ERROR,
+// Ele não mantém a conexão, só da um PING basicamente.
+try {
+    await sequelize.authenticate();
+    console.log('Conexão com o Banco de Dados Ok!!!');
+} catch (error) {
+    console.log('Erro ao se conectar com o Banco de Dados: ' + error);
+}
+
+export { sequelize };
